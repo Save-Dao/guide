b453d4965fa3fa02a5b679cfc6c8b1ec153f2595
LyghtCode <xikago.musica@gmail.com>
2022-03-16 17:14:30 -0600

    deploy changes

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/baba/grimoire/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/baba/grimoire/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/baba/grimoire/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/baba/grimoire/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Users/baba/grimoire/src/css/custom.css"),
-];
